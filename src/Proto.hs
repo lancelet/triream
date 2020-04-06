969c0b656436c0da736aa200c893f61a11378f44
Jonathan Merritt <j.s.merritt@gmail.com>
2020-04-06 20:20:07 +1000

    Quad type, and a test cube

--- src/Proto.hs
+++ src/Proto.hs
@@ -2,7 +2,7 @@
 module Proto where
 
 import           Linear                         ( V2(V2)
-                                                , V3
+                                                , V3(V3)
                                                 )
 import           Linear.Affine                  ( Point(P) )
 
@@ -13,11 +13,64 @@ data Tri v a
     , triP3 :: Point v a
     }
 
+data Quad v a
+  = Quad
+    { quadP1 :: Point v a
+    , quadP2 :: Point v a
+    , quadP3 :: Point v a
+    , quadP4 :: Point v a
+    }
+
 type P2 a = Point V2 a
+
+type P3 a = Point V3 a
 
 type Tri2 a = Tri V2 a
 
 type Tri3 a = Tri V3 a
+
+type Quad3 a = Quad V3 a
+
+-- | Construct a point from x,y,z coordinates.
+mkp3 :: a -> a -> a -> P3 a
+mkp3 x y z = P (V3 x y z)
+
+-- | Cube made of quadrilaterals.
+quadCube :: forall  a . Num a => [Quad3 a]
+quadCube =
+    [ Quad p1 p2 p3 p4
+    , Quad p5 p8 p7 p6
+    , Quad p1 p4 p8 p5
+    , Quad p2 p6 p7 p3
+    , Quad p1 p5 p6 p2
+    , Quad p3 p7 p8 p4
+    ]
+  where
+    p1, p2, p3, p4, p5, p6, p7, p8 :: P3 a
+    p1 = mkp3 1 1 1
+    p2 = mkp3 (-1) 1 1
+    p3 = mkp3 (-1) (-1) 1
+    p4 = mkp3 1 (-1) 1
+    p5 = mkp3 1 1 (-1)
+    p6 = mkp3 (-1) 1 (-1)
+    p7 = mkp3 (-1) (-1) (-1)
+    p8 = mkp3 1 (-1) (-1)
+
+
+-- | Cube made of triangles.
+triCube :: Num a => [Tri3 a]
+triCube = concatMap pairList $ splitQuad <$> quadCube
+  where
+    pairList :: (b, b) -> [b]
+    pairList (x, y) = [x, y]
+
+-- | Split a quad into its pair of triangles.
+splitQuad :: forall v a . Quad v a -> (Tri v a, Tri v a)
+splitQuad (Quad p1 p2 p3 p4) = (tri1, tri2)
+  where
+    tri1, tri2 :: Tri v a
+    tri1 = Tri p1 p2 p3
+    tri2 = Tri p1 p3 p4
 
 -- | Barycentric coordinates.
 data Bary a
