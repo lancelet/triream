ad50950462da450579006e4941009b0d1baf14d4
Jonathan Merritt <j.s.merritt@gmail.com>
2020-04-06 20:26:30 +1000

    Use pattern synonym for P2

--- src/Proto.hs
+++ src/Proto.hs
@@ -1,10 +1,9 @@
+{-# LANGUAGE PatternSynonyms     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 module Proto where
 
-import           Linear                         ( V2(V2)
-                                                , V3(V3)
-                                                )
-import           Linear.Affine                  ( Point(P) )
+import           Linear        (V2 (V2), V3 (V3))
+import           Linear.Affine (Point (P))
 
 data Tri v a
   = Tri
@@ -22,6 +21,10 @@ data Quad v a
     }
 
 type P2 a = Point V2 a
+
+pattern P2 :: a -> a -> P2 a
+pattern P2 x y <- P (V2 x y)
+{-# COMPLETE P2 #-}
 
 type P3 a = Point V3 a
 
@@ -82,10 +85,7 @@ data Bary a
 
 -- | Compute the barycentric coordinates of a point within a 2D triangle.
 bary :: forall  a . Fractional a => Tri2 a -> P2 a -> Bary a
-bary (Tri (P (V2 x1 y1)) (P (V2 x2 y2)) (P (V2 x3 y3))) (P (V2 x y)) = Bary
-    l1
-    l2
-    l3
+bary (Tri (P2 x1 y1) (P2 x2 y2) (P2 x3 y3)) (P2 x y) = Bary l1 l2 l3
   where
     l1, l2, l3 :: a
     l1 = ((y2 - y3) * dx + (x3 - x2) * dy) / det
